302a0b35683e9ced5c9777d7717855e94493b1b6
Rohit Yadav <rohity123456@gmail.com>
2024-04-27 19:47:08 +0530

    Refactor blog page component and remove unused code

--- src/blogs/page.tsx
+++ src/blogs/page.tsx
@@ -1,33 +1,9 @@
-// pages/blog/index.tsx
-import { GetStaticProps } from 'next';
-import { db } from '@/firebase'; // assuming you have this set up from previous steps
-import { collection, getDocs } from 'firebase/firestore';
+import React from 'react'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const blogPostCollection = collection(db, 'blogPosts');
-  const blogSnapshot = await getDocs(blogPostCollection);
-  const blogPosts = blogSnapshot.docs.map((doc) => doc.data());
-
-  return {
-    props: {
-      blogPosts
-    },
-    revalidate: 60 // In seconds (optional, for Incremental Static Regeneration)
-  };
-};
-
-const BlogPage = ({ blogPosts }) => {
+const BlogPage = () => {
   return (
-    <div>
-      <h1>Blog</h1>
-      {blogPosts.map((post, index) => (
-        <div key={index}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+    <div>BlogPage Coming soon !</div>
+  )
+}
 
-export default BlogPage;
+export default BlogPage
