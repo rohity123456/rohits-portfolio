880ca17f6d105325714e9bb371ac62535e43aff0
Rohit Yadav <rohity123456@gmail.com>
2024-04-28 21:08:26 +0530

    Update experience collection name in getExperiences function

--- src/service/experience.ts
+++ src/service/experience.ts
@@ -4,7 +4,7 @@ import { collection, getDocs } from 'firebase/firestore';
 
 export const getExperiences = async () => {
   try {
-    const experienceCollection = collection(firestoreDb, 'experience');
+    const experienceCollection = collection(firestoreDb, 'experiences');
     const experienceSnapshot = await getDocs(experienceCollection);
     const experiences = await experienceSnapshot.docs.map(
       (doc) => doc.data() as Experience
