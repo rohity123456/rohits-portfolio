7ddc2db5b439caf0c2283be028c17a3fbe94ad84
Rohit Yadav <rohity123456@gmail.com>
2024-04-28 21:56:21 +0530

    Add Loading component to display a progress bar

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <progress className='progress w-full -top-2.5 progress-primary'></progress>
+  );
+};
+
+export default Loading;
