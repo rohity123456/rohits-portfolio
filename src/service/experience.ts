4b65000173053c3d355b7badaf5b23eef8e6425e
Rohit Yadav <rohity123456@gmail.com>
2024-04-29 16:17:43 +0530

    Sort experiences by start date in getExperiences function

--- src/service/experience.ts
+++ src/service/experience.ts
@@ -9,6 +9,9 @@ export const getExperiences = async () => {
     const experiences = await experienceSnapshot.docs.map(
       (doc) => doc.data() as Experience
     );
+    experiences.sort((a, b) =>
+      new Date(b.startDate) > new Date(a.startDate) ? 1 : -1
+    );
     return experiences;
   } catch (error) {
     console.log('Error fetching experiences', error);
